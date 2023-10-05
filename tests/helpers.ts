04ed724e403aed89e37c04305b191a44e748312a
Kess220 <prodvictorkaio@gmail.com>
2023-10-05 10:04:09 -0300

    testes int e unit post booking

--- tests/helpers.ts
+++ tests/helpers.ts
@@ -5,16 +5,17 @@ import { createSession } from './factories/sessions-factory';
 import { prisma } from '@/config';
 
 export async function cleanDb() {
-  await prisma.address.deleteMany({});
+  await prisma.booking.deleteMany({});
+  await prisma.room.deleteMany({});
+  await prisma.hotel.deleteMany({});
   await prisma.payment.deleteMany({});
   await prisma.ticket.deleteMany({});
   await prisma.ticketType.deleteMany({});
+  await prisma.address.deleteMany({});
   await prisma.enrollment.deleteMany({});
-  await prisma.event.deleteMany({});
   await prisma.session.deleteMany({});
   await prisma.user.deleteMany({});
-  await prisma.room.deleteMany({});
-  await prisma.hotel.deleteMany({});
+  await prisma.event.deleteMany({});
 }
 
 export async function generateValidToken(user?: User) {
